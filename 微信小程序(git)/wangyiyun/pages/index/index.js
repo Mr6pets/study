b620d583c5f1d1d0f758ae98b9a6f424a69873dc
pet <1181702061@qq.com>
2019-09-30 20:07:38 +0800

    -a

--- 微信小程序(git)/wangyiyun/pages/index/index.js
+++ 微信小程序(git)/wangyiyun/pages/index/index.js
@@ -2,14 +2,16 @@
 //轮播数据拿取
 var swiper=require('../../data/swiperData.js');
 //引入新闻内容数据
-var swiper = require('../../data/swiperData.js');
+var newslist = require('../../data/newslist.js');
 
 Page({
   data: {
     //设置一个属性 赋值一个初始状态 是否选中 classs是否添加 三元判断
     //点击事件 点击谁 谁的值赋值给selected
     selected:'yaowen',
-    swiperData:swiper.swiperData
+    swiperData:swiper.swiperData,
+    newslist: newslist.sData
+
   },
   //函数--实现导航选中的切换
   change:function(event){
@@ -23,6 +25,7 @@ Page({
   },
   onLoad:function(){
     // console.log(swiper);
+    console.log(newslist);
   },
   //如果是后台请求接口 用微信自带的方式 wx.require({})
 })
