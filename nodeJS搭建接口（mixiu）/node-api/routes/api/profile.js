abf04d76f116338c05f87407e79bc3459ebd25be
liuan <1761330769@qq.com>
2019-11-23 05:32:58 +0800

    ..

--- nodeJS搭建接口（mixiu）/node-api/routes/api/profile.js
+++ nodeJS搭建接口（mixiu）/node-api/routes/api/profile.js
@@ -6,14 +6,14 @@ const mongoose=require("mongoose");
 const passport=require("passport");
 
 //引入了2张表
-const Profile=require("../../models/Profile");
+const Profile=require("../../models/Profiles");
 const User=require("../../models/Users.js")
 
 //$route GET api/profile/test
 //@desc 返回的请求的json数据
 //@access public
-router.post("/test",(req,res)=>{
-	res.json({msg:"done"})
+router.get("/test",(req,res)=>{
+	res.json({msg:"donedone"})
 });
 
 //$route GET api/profile
