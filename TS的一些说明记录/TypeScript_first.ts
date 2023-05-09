c5cb366e8bcbea2afb18c9e59c47c50461416469
la <alvispets@gmail.com>
2023-05-09 14:10:35 +0800

    add some new files in my store

---
+++ TS的一些说明记录/TypeScript_first.ts
@@ -0,0 +1,81 @@
+//基础的使用
+
+// function getters(person:string){
+// 	return "hello ,"+person;
+// }
+// let user="alvis";
+
+// document.body.innerHTML=getters(user);
+
+
+
+// interface Person{
+// 	firstName:string;
+// 	lastName:string;
+// }
+
+// function greeter(person:Person){
+// 	return "Hello, "+person.firstName+" "+person.lastName;
+// }
+
+// let user =new Student("alvis",'liu','user');
+// document.body.innerHTML=greeter(user);
+
+
+// 面相对象
+//创建一个Student的类(在构造函数的参数上使用public等同于创建了同名的成员变量。)
+// class Student{
+// 	fullName:string,
+// 	constructor(public firstName:string,public middleName:string,public lastName:string){
+// 		this.fullName=firstName+''+middleName+''+lastName;
+// 	}
+// }
+
+
+
+// 基础类型的说明
+
+//定义数组
+let list:number[]=[1,2,3];
+let list:Array<number>=[1,2,3];
+
+
+let list:number=[1,2,3];
+let list:Array<number>=[1,2,3];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
